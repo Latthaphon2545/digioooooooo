be940329b105c91da4635bd55483282668f108d5
Latthaphon Phoemmanirat <108315515+Latthaphon2545@users.noreply.github.com>
2024-05-16 10:53:25 +0700

    Test

--- src/app/Game/page.tsx
+++ src/app/Game/page.tsx
@@ -3,6 +3,9 @@ export default function Home() {
     <>
       <h1>Game</h1>
       <p>Game page content</p>
+      <button className="btn btn-primary  btn-outline">One</button>
+      <button className="btn btn-secondary  btn-outline">Two</button>
+      <button className="btn btn-accent btn-outline">Three</button>
     </>
   );
 }
