c1ae8ae13e55d12193012af7e597aef0120fe175
Latthaphon Phoemmanirat <108315515+Latthaphon2545@users.noreply.github.com>
2024-05-16 14:51:33 +0700

    table,pagination

--- src/app/Game/page.tsx
+++ src/app/Game/page.tsx
@@ -1,11 +1,9 @@
+import Table from "@/components/table/table";
+
 export default function Home() {
   return (
     <>
-      <h1>Game</h1>
-      <p>Game page content</p>
-      <button className="btn btn-primary  btn-outline">One</button>
-      <button className="btn btn-secondary  btn-outline">Two</button>
-      <button className="btn btn-accent btn-outline">Three</button>
+      <Table />
     </>
   );
 }
