15ed9d6d358118b663f9abfd0481f4b5eac112fc
Latthaphon Phoemmanirat <108315515+Latthaphon2545@users.noreply.github.com>
2024-05-17 01:03:09 +0700

    up

--- src/app/Game/page.tsx
+++ src/app/Game/page.tsx
@@ -1,9 +1,18 @@
-import Table from "@/components/table/table";
+import Table from "@/components/table/tablePage";
+import { randomInt } from "crypto";
 
 export default function Home() {
+  const data = Array.from({ length: 33 }, (_, i) => ({
+    name: `Person ${i + 1}`,
+    email: `person${i + 1}@digio.co.th`,
+    role: ["Admin", "Operator", "Call Center"][randomInt(0, 2)],
+    status: ["Pending", "Active", "Restricted", "Inactive"][randomInt(0, 3)],
+    Contact: randomInt(1000000000, 9999999999),
+  }));
+
   return (
     <>
-      <Table />
+      <Table data={data} colorStatus="user" />
     </>
   );
 }
--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -1,80 +1,53 @@
-"use client";
+import React from "react";
 
-import { useState } from "react";
-import Pagination from "./pagination";
+interface TableProps {
+  dataForCurrentPage: {
+    [key: string]: any;
+  }[];
+  colorUserStatus: (status: string) => string;
+}
 
-export default function Table() {
-    const data = Array.from({ length: 100 }, (_, i) => ({
-        name: `Person ${i + 1}`,
-        email: `person${i + 1}@digio.co.th`,
-        role: i % 2 === 0 ? "Admin" : "Operator",
-        status: ["Pending", "Active", "Restricted", "Inactive"][i % 4],
-        Contact: `081-234-567${i % 10}`
-    }));
-
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 8;
-    const totalPages = Math.ceil(data.length / itemsPerPage);
-    let dataForCurrentPage = data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
-
-    const colorUserStatus = (status: string) => {
-        status = status.toLocaleLowerCase()
-        let color = ""
-        if (status === "active") {
-            color = "success"
-        } else if (status === "inactive") {
-            color = ""
-        } else if (status === "restricted") {
-            color = "error"
-        } else if (status === "pending") {
-            color = "neutral"
-        }
-        return color
-    }
-
-    const onPageChange = (pageNumber: number) => {
-        setCurrentPage(pageNumber);
-    }
-
-    return (
-        <>
-            <div className="container mx-auto my-5 px-4 py-4 border-2 border-black">
-                <div className="overflow-x-auto">
-                    <table className="table table-zebra">
-                        <thead>
-                            <tr className="bg-base-200 text-center">
-                                <th>Name</th>
-                                <th>Role</th>
-                                <th>Status</th>
-                                <th>Contact</th>
-                            </tr>
-                        </thead>
-                        {dataForCurrentPage.map((item) => {
-                            return (
-                                <tbody key={item.name}>
-                                    <tr>
-                                        <td>
-                                            <span>
-                                                <p className="text-base">{item.name}</p>
-                                                <p className="text-xs text-gray-500">{item.email}</p>
-                                            </span>
-                                        </td>
-                                        <td className="text-center">{item.role}</td>
-                                        <td className="text-center">
-                                            <span className={`badge badge-${colorUserStatus(item.status)} badge-outline`}>{item.status}</span>
-                                        </td>
-                                        <td className="text-center">{item.Contact}</td>
-                                    </tr>
-                                </tbody>
-                            )
-                        }
-                        )}
-                    </table>
-                </div>
-                <div className="flex justify-center mt-5">
-                    <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} />
-                </div>
-            </div>
-        </>
-    )
+export default function Table({
+  dataForCurrentPage,
+  colorUserStatus,
+}: TableProps) {
+  return (
+    <div className="min-h-[70vh]">
+      <table className="table">
+        <thead>
+          <tr className="bg-base-200 text-center">
+            <th className="min-w-[18vw]">Name</th>
+            <th>Role</th>
+            <th>Status</th>
+            <th>Contact</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataForCurrentPage.map((item) => {
+            return (
+              <tr className="content-start">
+                <td>
+                  <span>
+                    <p className="text-base">{item.name}</p>
+                    <p className="text-xs text-gray-500">{item.email}</p>
+                  </span>
+                </td>
+                <td className="text-center">{item.role}</td>
+                <td className="text-center">
+                  <span
+                    className={`badge badge-${colorUserStatus(
+                      item.status
+                    )} badge-outline`}
+                  >
+                    {item.status}
+                  </span>
+                </td>
+                <td className="text-center">{item.Contact}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
